bf10d6788b957628b8f837652a18a131e751ebe8
Josh Elias <josh.elias.dev@gmail.com>
2016-04-25 13:32:54 -0700

    fixed index.js

--- aws-ses.js
+++ aws-ses.js
@@ -1,4 +1,3 @@
-
 var AWS = require("aws-sdk");
 var Promise = require("bluebird");
 var loopback = require('loopback');
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/mandrill');
+module.exports = require("./aws-ses");
